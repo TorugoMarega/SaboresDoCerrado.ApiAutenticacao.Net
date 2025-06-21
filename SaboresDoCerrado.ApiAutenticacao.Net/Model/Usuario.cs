c2535fe4911dc6ef50b0ce0a8ae0a956ae24564e
Victor Hugo Marega <victorhugomarega@hotmail.com>
2025-06-21 14:13:10 -0300

    Criação das tabelas base, configuração do DB, migrations, e endpoint de healthcheck

---
+++ SaboresDoCerrado.ApiAutenticacao.Net/Model/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace SaboresDoCerrado.ApiAutenticacao.Net.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string HashSenha { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+        public ICollection<UsuarioPerfil> UsuarioPerfil { get; set; }
+    }
+}
---
+++ SaboresDoCerrado.ApiAutenticacao.Net/Model/UsuarioPerfil.cs
@@ -0,0 +1,10 @@
+﻿namespace SaboresDoCerrado.ApiAutenticacao.Net.Model
+{
+    public class UsuarioPerfil
+    {
+      public int UsuarioId  { get; set; }
+      public int PerfilId  { get; set; }
+      public Usuario Usuario  { get; set; }
+      public Perfil Perfil  { get; set; }
+}
+}
